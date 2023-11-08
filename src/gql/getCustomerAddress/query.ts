1c4baddad3d6e9ef8e81eec3c0060ddf90577de3
Tim Stone <tim@lush.co.uk>
2023-11-08 12:24:51 +0000

    Add address retrieval

---
+++ src/gql/getCustomerAddress/query.ts
@@ -0,0 +1,25 @@
+import { gql } from "graphql-request";
+
+export default gql`
+query getCustomerAddress($id: ID) {
+  user(id: $id) {
+    addresses(first: 1, sortBy: CREATED_AT_DESC) {
+      edges {
+        node {
+          id
+          firstName
+          lastName
+          addressLine1
+          addressLine2
+          city
+          country {
+            code
+            country
+          }
+          postalCode
+          phone
+        }
+      }
+    }
+  }
+}`;
